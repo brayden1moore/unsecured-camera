e49c0ed06e9aef103d35aed5916ad8763182be86
Brayden Moore <braydenmoore2001@gmail.com>
2023-09-05 21:53:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,16 @@ from urllib.parse import urlparse, parse_qs
 
 @app.route('/proxy/<path:url>')
 def proxy(url):
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Dnt': '1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    }
     try:
         # Remove 'proxy/' from the url if present
         clean_url = url.replace('proxy/', '')
