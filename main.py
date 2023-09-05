12b28f76b2e5ed0bf78aac271d36bb4b5a6174c1
Brayden Moore <braydenmoore2001@gmail.com>
2023-09-05 22:58:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def proxy(url):
         
         content_type = req.headers['content-type']
         
-        return Response(req.iter_content(chunk_size=10*1024), content_type=content_type)
+        return Response(req.iter_content(chunk_size=10*1024), content_type=content_type, headers={"Connection": "close"})
         
     except requests.exceptions.RequestException as e:
         print(f'MY Error: {e}')
